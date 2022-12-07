e39e79da06e89d618c68ac266c88ab54622f4ab9
Alejandro D. Guevara <47149928+wandu-ar@users.noreply.github.com>
2022-12-07 15:45:07 -0300

    oks

--- src/functions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './intersection-types';
-export * from './intersection-type';
-export * from './omit-type';
-export * from './pick-type';
-export * from './partial-type';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './types';
+export * from './lib/storage';
+// export * from './decorators/schema-scanner.decorator';
