47795620c817c9ef9197eb0ea78789c4d104ffe9
Alejandro D. Guevara <47149928+wandu-ar@users.noreply.github.com>
2022-12-11 17:31:43 -0300

    new deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-npm run build
-npm version patch
-git push
-git push --tags
-npm publish --access=public
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  read -p "Please, input version: " VERSION
+fi
+
+npm run build && npm version $VERSION && git push && git push --tags && npm publish --access=public
