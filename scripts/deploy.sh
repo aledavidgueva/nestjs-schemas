0bbce8ce67bdef3dffd54991a52fe21f57f931d9
Alejandro D. Guevara <47149928+wandu-ar@users.noreply.github.com>
2022-12-14 13:35:14 -0300

    add chain

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ if [ -z "$VERSION" ]; then
   read -p "Please, input version: " VERSION
 fi
 
-npm run build
-npm version $VERSION
-git push
-git push --tags
+npm run build && \
+npm version $VERSION && \
+git push && \
+git push --tags && \
 npm publish --access=public
